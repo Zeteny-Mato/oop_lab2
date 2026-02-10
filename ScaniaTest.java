6016e11b4038b70308efb4d699a48103f94edbca
Zéti <zeti.mato@gmail.com>
2026-02-10 15:59:47 +0100

    ScaniaTest

---
+++ ScaniaTest.java
@@ -0,0 +1,25 @@
+import org.junit.jupiter.api.Test;
+
+import java.awt.*;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+
+class ScaniaTest {
+    private final Scania scania = new Scania();
+
+    @Test
+    void getNrDoors() {
+        assertEquals(2,scania.getNrDoors());
+    }
+
+    @Test
+    void getEnginePower() {
+        assertEquals(400,scania.getEnginePower());
+    }
+
+    @Test
+    void getColor() {
+        assertEquals(Color.RED, scania.getColor());
+    }
+}
